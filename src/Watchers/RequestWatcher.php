fdd295e64c505a50b4eac7b44ae76efb2f7682a1
Mantas <mantas783@gmail.com>
2020-01-26 14:55:44 +0200

    Group url's with different parameters

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -9,12 +9,19 @@ class RequestWatcher
     {
         $duration = microtime(true) - LARAVEL_START;
 
+        $route = \Request::route();
+        if ($route !== null) {
+            $name = $route->uri();
+        } else {
+            $name = request()->path();
+        }
+
         LogWriter::log(
             round(LARAVEL_START),
             $duration,
             QueryWatcher::getMilliseconds() / 1000,
             'request',
-            request()->path(),
+            $name,
             \Auth::check() ? \Auth::user()->email : request()->ip()
         );
     }
