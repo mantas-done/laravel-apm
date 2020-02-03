a3ad3a3769ae30243081ccd61a9beaa2469a619d
Mantas <mantas783@gmail.com>
2020-02-03 21:25:39 +0200

    Add grouping by user

--- src/LogParser.php
+++ src/LogParser.php
@@ -25,7 +25,11 @@ class LogParser
         $data = \File::get($path);
         $data = trim($data);
 
-        preg_match_all('/^([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ \n]+)/m', $data, $matches, PREG_SET_ORDER);
+        $pattern = '/^([^\s]+) ([^\s]+) ([^\s]+) ([^\s]+) ([^\s]+)/m';
+        if ($group === 'user') {
+            $pattern = '/^([^\s]+) ([^\s]+) ([^\s]+) ([^\s]+) ([^\s]+) ([^\s]+)/m';
+        }
+        preg_match_all($pattern, $data, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $record) {
             // $timestamp, $duration, $sql_duration, $type, $name, $ip
@@ -52,7 +56,7 @@ class LogParser
                 }
                 $top_requests[$record[5]] += $record[3];
             } elseif ($group === 'request-count') {
-                $count_by_hour[$hour . 'h'] += 1;
+                $count_by_hour[$hour . 'h']++;
 
                 if (!isset($top_requests[$record[5]])) {
                     $top_requests[$record[5]] = 0;
@@ -69,6 +73,17 @@ class LogParser
                 if ($top_requests[$record[5]] < $record[2]) {
                     $top_requests[$record[5]] = $record[2];
                 }
+            } elseif ($group === 'user') {
+                if ($record[4] !== 'request') {
+                    break;
+                }
+
+                $count_by_hour[$hour . 'h'] += 1;
+
+                if (!isset($top_requests[$record[6]])) {
+                    $top_requests[$record[6]] = 0;
+                }
+                $top_requests[$record[6]]++;
             } else {
                 throw new \Exception('unknown group');
             }
@@ -82,6 +97,8 @@ class LogParser
             $top_total_count = array_sum($count_by_hour);
         } elseif ($group === 'longest') {
             $top_total_count = count($top_requests) ? max($top_requests) : 0;
+        } elseif ($group === 'user') {
+            $top_total_count = array_sum($count_by_hour);
         } else {
             throw new \Exception('unknown group');
         }
